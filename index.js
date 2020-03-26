713f0cc23ea16a5a127433fff103cf794e414398
Doga Yuksel <dogayuksel@pm.me>
2020-03-26 12:26:01 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-import * as wasm from "hello-wasm-pack";
+import { FuzzyMatcher } from "wasm-fuzzy-match";
 
-wasm.greet();
+const phrases = [
+  "Let me freshin' it up little one on one with no ruff (let's go)",
+  "When I hear what they have to say about me",
+  "I know that he loves me cause he told me so",
+];
+
+const matcher = new FuzzyMatcher(phrases);
+const result = matcher.query("know", 0);
+
+console.log(result);
